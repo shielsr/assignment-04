9a81ab1c2e57c11027291af5c306519e10beb024
Rob Shiels <rob@robshiels.com>
2025-10-30 20:54:38 +0000

    Setting up the Flask app and initial templates

---
+++ app.py
@@ -0,0 +1,22 @@
+import os
+from flask import Flask, render_template, request, redirect, url_for, session
+from models import *
+
+
+app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY", "fallback-secret-key") # For doing the logins
+
+@app.route('/')
+def index():
+    # Render the home page with all gigs
+    return render_template('index.html')
+
+@app.route('/create', methods=["GET"])
+def create():
+    # Render the 'Create' page
+    return render_template('create.html')
+
+
+
+if __name__ == '__main__':
+     app.run(debug=True, port=8000)
